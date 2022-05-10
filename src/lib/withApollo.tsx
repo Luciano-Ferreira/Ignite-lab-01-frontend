edba23a0c1b57a499a1c0c46762e430ecd4e287c
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-10 19:35:55 -0300

    feat: cache for ssr

--- src/lib/withApollo.tsx
+++ src/lib/withApollo.tsx
@@ -1,24 +1,24 @@
-import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import { NextPage } from 'next';
 
 // HOC -  High Order Component
 export const withApollo = (Component: NextPage) => {
   return function Provider(props: any) {
     return (
-      <ApolloProvider client={getApolloClient()}>
+      <ApolloProvider client={getApolloClient(props.apolloState)}>
         <Component {...props} />
       </ApolloProvider>
     )
   }
 }
 
-function getApolloClient() {
+function getApolloClient(ssrCache?: NormalizedCacheObject) {
   const httpLink = createHttpLink({
     uri: 'http://localhost:3332/graphql',
     fetch
   });
   
-  const cache = new InMemoryCache();
+  const cache = new InMemoryCache().restore(ssrCache ?? {})
   
   return new ApolloClient({
     link: from([httpLink]),
